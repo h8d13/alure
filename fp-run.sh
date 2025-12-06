aaa2703c3f92e739c8901e845ad2ec688188ca1a
h8d13 <hadrien.loge@gmail.com>
2025-12-06 13:09:00 +0100

    rem redundant

--- fp-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# run flatpak app wrap 
-[ -z "$1" ] && { echo "Usage: fp run <appname>"; exit 1; }
-app=$(flatpak list --app --columns=application | grep -i "$1" | head -1)
-[ -z "$app" ] && { echo "No app matching '$1' found"; exit 1; }
-flatpak run "$app"
